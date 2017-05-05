845ed8646733a23f57a3135948d9c2604f48f562
Shuyue Xue <sx2174@columbia.edu>
2017-05-05 19:17:19 +0000

    Replace Test_f1.py

--- tests/Test_f1.py
+++ tests/Test_f1.py
@@ -1,14 +1,15 @@
 import sys  
 sys.path.append('../splot')
-from splot1 import *
+from splot1Py3 import *
 
-# Load Data Set 1:
+#### Load Data Set 1:
 r1 = np.loadtxt("../data/examples/f1/80", skiprows=4, usecols=(0,))
 Gtrunc1 = np.loadtxt("../data/examples/f1/80", skiprows=4, usecols=(1,))
 Gdiff1 = np.loadtxt("../data/examples/f1/80", skiprows=4, usecols=(2,))
 Gcalc1 = np.loadtxt("../data/examples/f1/80", skiprows=4, usecols=(3,))
 
 # Load Data Set 2:
+"../data/examples/f1/10"
 r2 = np.loadtxt("../data/examples/f1/10", skiprows=4, usecols=(0,))
 Gtrunc2 = np.loadtxt("../data/examples/f1/10", skiprows=4, usecols=(1,))
 Gdiff2 = np.loadtxt("../data/examples/f1/10", skiprows=4, usecols=(2,))
@@ -23,10 +24,10 @@ g2 = Data( (r2, Gtrunc2), samplename ='G2', scan = 'Orginal', color = c[1], mark
 g2calc = Data( (r2, Gcalc2), samplename = 'G2', scan = 'Calc2', color = c[3], line='--', marker = "")
 g2diff = Data( (r2, Gdiff2-8), samplename = 'G2', scan = 'Diff', color = 'r', marker = "")
 
-# plot axis setup: 2 rows x 1 col
+# plot axis setup: here we have 2 rows x 1 col
 H = Splot(2,1)
 
-# plot data 
+# plot data
 H.plotData(g1, 0, 0)    
 H.plotData(g1calc, 0, 0, diff = True)
 H.plotData(g1diff, 0, 0)
@@ -36,9 +37,8 @@ H.plotData(g2calc, 1, 0, diff = True)
 H.plotData(g2diff, 1, 0)
 
 #Add Title, label
-H.title("G(r) Plot", math = 'on')
-H.label(x = "hello", xunit = 'delta', y = 'world', yunit = 'epsilon', math = 'on')
+H.title("G(r) Plot", math = "off")
+#H.label(x = "hello", xunit = 'delta', y = 'world', yunit = 'epsilon', math = 'on')
 
 #Save figure:
-H.save(name = "myfigure", form = "pdf")
-plt.show()
+#H.save(name = "myfigure", form = "pdf")
