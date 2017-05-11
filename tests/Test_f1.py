c7e01543f9e691b3c126de07094e50c070fdadad
Shuyue Xue <sx2174@columbia.edu>
2017-05-11 00:49:38 +0000

    Add dataPath variable.

--- tests/Test_f1.py
+++ tests/Test_f1.py
@@ -2,47 +2,45 @@ import sys
 sys.path.append('../splot')
 from splot1Py3 import *
 
+#get the dataPaths:
+dataPath1 = "../data/examples/f1/80"
+dataPath2 = "../data/examples/f1/10"
+
 #### Load Data Set 1:
-r1 = np.loadtxt("../data/examples/f1/80", skiprows=4, usecols=(0,))
-Gtrunc1 = np.loadtxt("../data/examples/f1/80", skiprows=4, usecols=(1,))
-Gdiff1 = np.loadtxt("../data/examples/f1/80", skiprows=4, usecols=(2,))
-Gcalc1 = np.loadtxt("../data/examples/f1/80", skiprows=4, usecols=(3,))
+r1 = np.loadtxt(dataPath1, skiprows=4, usecols=(0,))
+Gtrunc1 = np.loadtxt(dataPath1, skiprows=4, usecols=(1,))
+Gdiff1 = np.loadtxt(dataPath1, skiprows=4, usecols=(2,))
+Gcalc1 = np.loadtxt(dataPath1, skiprows=4, usecols=(3,))
 
 # Load Data Set 2:
-"../data/examples/f1/10"
-r2 = np.loadtxt("../data/examples/f1/10", skiprows=4, usecols=(0,))
-Gtrunc2 = np.loadtxt("../data/examples/f1/10", skiprows=4, usecols=(1,))
-Gdiff2 = np.loadtxt("../data/examples/f1/10", skiprows=4, usecols=(2,))
-Gcalc2 = np.loadtxt("../data/examples/f1/10", skiprows=4, usecols=(3,))
+r2 = np.loadtxt(dataPath2, skiprows=4, usecols=(0,))
+Gtrunc2 = np.loadtxt(dataPath2, skiprows=4, usecols=(1,))
+Gdiff2 = np.loadtxt(dataPath2, skiprows=4, usecols=(2,))
+Gcalc2 = np.loadtxt(dataPath2, skiprows=4, usecols=(3,))
 
-#Setup Data for the plot:
-#g1 = Data( (r1, Gtrunc1), samplename ='G1', scan = 'Orginal', color = c[0], marker = '+')
-g1 = Data( (r1, Gtrunc1), samplename ='G1', scan = 'Orginal', color = c[1])
-#g1calc = Data( (r1, Gcalc1), samplename  = 'G1', scan = 'Calc', color = c[4], line='--', marker = "")
-g1calc = Data( (r1, Gcalc1), samplename  = 'G1', scan = 'Calc', color = 'r')
-g1diff = Data( (r1, Gdiff1-8), samplename = 'G1', scan = 'Diff', color = c[0])
-
-g2 = Data( (r2, Gtrunc2), samplename ='G2', scan = 'Orginal', color = c[2])
-g2calc = Data( (r2, Gcalc2), samplename = 'G2', scan = 'Calc2', color = 'r')
-g2diff = Data( (r2, Gdiff2-8), samplename = 'G2', scan = 'Diff', color = c[0])
-
-#Example of not reseting the marker / color / linestyle, unless the user wants to
+#Setup Data for the plot: User doesn't have to set up the marker / color / linestyle unless they want
 g1 = Data( (r1, Gtrunc1), samplename ='G1', scan = 'Orginal')
-#g1 = Data( (r1, Gtrunc1), samplename ='G1', scan = 'Orginal', color = c[0], marker = '+')
 g1diff = Data( (r1, Gdiff1-8), samplename = 'G1', scan = 'Diff')
-#g1diff = Data( (r1, Gdiff1-8), samplename = 'G1', scan = 'Diff', color = c[0])
-g1calc = Data( (r1, Gcalc1), samplename  = 'G1', scan = 'Calc', color = 'r')
-#g1calc = Data( (r1, Gcalc1), samplename  = 'G1', scan = 'Calc', color = c[4], line='--', marker = "")
-#g1calc = Data( (r1, Gcalc1), samplename  = 'G1', scan = 'Calc', color = c[4])
+g1calc = Data( (r1, Gcalc1), samplename  = 'G1', scan = 'Calc')
+
+g2 = Data( (r2, Gtrunc2), samplename ='G2', scan = 'Orginal')
+g2calc = Data( (r2, Gcalc2), samplename = 'G2', scan = 'Calc2')
+g2diff = Data( (r2, Gdiff2-8), samplename = 'G2', scan = 'Diff')
 
 # plot axis setup: here we have 2 rows x 1 col
 H = Splot(2,1)
+
+#Test the setLine(), the function for user to customize the linestyle:
+H.setLine(g1, color = c[2], marker = '+', line = '')
+H.setLine(g1calc, color = c[6], line = '--', marker = '')
+H.setLine(g1diff, color = c[8])
 
 # plot data
 H.plotData(g1, 0, 0)    
 H.plotData(g1calc, 0, 0, diff = True)
 H.plotData(g1diff, 0, 0)
 
+#g2 data uses default set from the group style sheet 
 H.plotData(g2, 1, 0)
 H.plotData(g2calc, 1, 0, diff = True)
 H.plotData(g2diff, 1, 0)
@@ -52,4 +50,4 @@ H.title("G(r) Plot", math = "off")
 #H.label(x = "hello", xunit = 'delta', y = 'world', yunit = 'epsilon', math = 'on')
 
 #Save figure:
-H.save(name = "myfigure", form = "pdf")
+#H.save(name = "myfigure", form = "pdf")
