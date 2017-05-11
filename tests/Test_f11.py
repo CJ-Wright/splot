bd479e2e320fad3011ef9a8076da8e9ee4a84956
Shuyue Xue <sx2174@columbia.edu>
2017-05-11 00:55:07 +0000

    Update for using the new splot.

--- tests/Test_f11.py
+++ tests/Test_f11.py
@@ -7,16 +7,16 @@ H = Splot()
 
 for i in range (11):
     #load data:
-    lc = "../data/examples/f11/"+str(i*10)
-    r = np.loadtxt(lc, skiprows = 4, usecols=(0,))
-    gtrunc = np.loadtxt(lc, skiprows = 4, usecols=(1,))
+    dataPath = "../data/examples/f11/"+str(i*10)
+    r = np.loadtxt(dataPath, skiprows = 4, usecols=(0,))
+    gtrunc = np.loadtxt(dataPath, skiprows = 4, usecols=(1,))    
     
-    #Set up data to be plot:
+    #Set up data to be plot
     G = Data((r, gtrunc),  samplename = "Gtrunc"+"_"+str(i*10), \
-    scan = 'Data', color = c[i], marker = '')
-    
+    scan = 'Data')
+
 #plot data: x offset is 10 units right to the previous curve, y offset is 10 units above the previous 
     H.plotData(G, scal=1, offsetx = i*10, offsety = i*10,)
     
 #Title is default to be empty, lable is default: G v.s r 
-#H.title("G(r) Plot")
+#H.title("G(r) Plot", math = "off")
