66106c242087285e6c0d924f2979fdfa81c644c8
Shuyue Xue <sx2174@columbia.edu>
2017-05-12 03:25:54 +0000

    Update for the new Data dictionary.

--- tests/Test_f27.py
+++ tests/Test_f27.py
@@ -1,6 +1,6 @@
-import sys, os
+#!/Users/Shuyue/mc/envs/py27/bin/python
+import sys
 sys.path.append('../splot')
-from path import path
 from splot1Py3 import *
 
 # plot axis setup: a single water fall plot 
@@ -16,11 +16,11 @@ for i in r:
     
     #Set up data
     G = Data((r, gtrunc),  samplename = "Gtrunc"+"_"+str(i), \
-                scan = 'Data')
+                scan = 'Data', marker = 'o', line = '')
     Gcalc = Data((r, gtrunc),  samplename = "Gtrunc"+"_"+str(i), \
                 scan = 'Calculated')
 
     #plot data: y offset is set to be same as the file name, x offset is -1. 
     H.plotData(G, offsetx = -1, offsety = i)
     #plot calculated curve at the same position 
-    H.plotData(Gcalc, offsetx = -1, offsety = i,)
+    H.plotData(Gcalc, offsetx = -1, offsety = i)
