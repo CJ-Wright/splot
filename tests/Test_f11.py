e66806b1b28c627e946106b604b4e7e33414eaf9
Simon Billinge <simon.billinge@gmail.com>
2017-05-13 14:28:44 -0400

    small tweaks

--- tests/Test_f11.py
+++ tests/Test_f11.py
@@ -3,7 +3,8 @@ sys.path.append('../splot')
 from splot1Py3 import *
 
 # plot axis setup: a single water fall plot 
-H = Splot()
+H = Splot(1
+mpl.rcParams['lines.linewidth'] = 2
 
 for i in range (11):
     #load data:
@@ -13,10 +14,13 @@ for i in range (11):
     
     #Set up data to be plot
     G = Data((r, gtrunc),  samplename = "Gtrunc"+"_"+str(i*10), \
-    scan = 'Data')
+    scan = 'Data', line = '-')
 
 #plot data: x offset is 10 units right to the previous curve, y offset is 10 units above the previous 
-    H.plotData(G, scal=1, offsetx = i*10, offsety = i*10,)
+#    H.plotData(G, scal=1, offsetx = i*10, offsety = i*10,)
+    H.plotData(G, scal=1, offsety = i*-0.5,)
     
 #Title is default to be empty, lable is default: G v.s r 
 #H.title("G(r) Plot", math = "off")
+#H.save(name = "myfigure", form = "pdf")
+plt.show()
