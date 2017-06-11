020a7d7a1f78c8eced9f91a1bc4dabfc5c820f68
Shuyue Xue <sx2174@columbia.edu>
2017-06-11 05:00:09 +0000

    Replace Test_f27.py for the outside legend

--- tests/Test_f27.py
+++ tests/Test_f27.py
@@ -3,7 +3,7 @@ import sys
 sys.path.append('../splot')
 from splot1Py3 import *
 
-# plot axis setup: a single water fall plot 
+# plot axis setup: a single water fall plot
 H = Splot()
 
 r = [0, 20, 50, 80, 100]
@@ -11,16 +11,18 @@ for i in r:
     dataPath = "../data/examples/f27/"+str(i)
     r = np.loadtxt(dataPath, skiprows = 4, usecols=(0,))
     gtrunc = np.loadtxt(dataPath, skiprows = 4, usecols=(1,))
-#    gdiff = np.loadtxt(dataPath, skiprows = 4, usecols=(2,))    #will not be plotted 
-    gcalc = np.loadtxt(dataPath, skiprows = 4, usecols=(3,))    
-    
+#    gdiff = np.loadtxt(dataPath, skiprows = 4, usecols=(2,))    #will not be plotted
+    gcalc = np.loadtxt(dataPath, skiprows = 4, usecols=(3,))
+
     #Set up data
-    G = Data((r, gtrunc),  samplename = "Gtrunc"+"_"+str(i), \
+    G = data((r, gtrunc),  samplename = "Gtrunc"+"_"+str(i), \
                 scan = 'Data', marker = 'o', line = '')
-    Gcalc = Data((r, gtrunc),  samplename = "Gtrunc"+"_"+str(i), \
+    Gcalc = data((r, gtrunc),  samplename = "Gtrunc"+"_"+str(i), \
                 scan = 'Calculated')
 
-    #plot data: y offset is set to be same as the file name, x offset is -1. 
+    #plot data: y offset is set to be same as the file name, x offset is -1.
     H.plotData(G, offsetx = -1, offsety = i)
-    #plot calculated curve at the same position 
-    H.plotData(Gcalc, offsetx = -1, offsety = i)
+    #plot calculated curve at the same position
+    H.plotData(Gcalc, offsetx = -1, offsety = i, legend = 'out')
+
+H.figureSize(8, 8)
