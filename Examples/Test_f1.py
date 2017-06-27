94d1f2f08a14953ef53436128179f04bc591c69a
Shuyue Xue <sx2174@columbia.edu>
2017-06-27 22:59:19 +0000

    Example for plotting f1 data.

--- Examples/Test_f1.py
+++ Examples/Test_f1.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.append('../splot')
-from splot1Py3 import *
+from splot import *
 
 #get the dataPaths:
 dataPath1 = "../data/examples/f1/80"
@@ -18,54 +18,32 @@ Gtrunc2 = np.loadtxt(dataPath2, skiprows=4, usecols=(1,))
 Gdiff2 = np.loadtxt(dataPath2, skiprows=4, usecols=(2,))
 Gcalc2 = np.loadtxt(dataPath2, skiprows=4, usecols=(3,))
 
-#Setup Data for the plot:
-#g1 = data( (r1, Gtrunc1), samplename ='G1', scan = 'Orginal', color = c[0], marker = '+')
-g1 = data( (r1, Gtrunc1), samplename ='G1', scan = 'Orginal', color = c[1], marker = 'o')
-#g1calc = data( (r1, Gcalc1), samplename  = 'G1', scan = 'Calc', color = c[4], line='--', marker = "")
-g1calc = data( (r1, Gcalc1), samplename  = 'G1', scan = 'Calc', color = 'r')
-g1diff = data( (r1, Gdiff1), samplename = 'G1', scan = 'Diff', color = c[0])
+# Setup Data for the plot: User can set line style
+g1 = data_dict( (r1, Gtrunc1), samplename ='G1', scan = 'Orginal', color = c[8], marker = 'o', linestyle = ":")
+#g1 = data_dict( (r1, Gtrunc1), samplename ='G1', scan = 'Orginal')
 
-g2 = data( (r2, Gtrunc2), samplename ='G2', scan = 'Orginal', color = c[2], marker = 'o')
-g2calc = data( (r2, Gcalc2), samplename = 'G2', scan = 'Calc2', color = 'r')
-g2diff = data( (r2, Gdiff2), samplename = 'G2', scan = 'Diff', color = c[0])
+g1diff = data_dict( (r1, Gdiff1), samplename = 'G1', scan = 'Diff', color = 'r', linestyle = "--")
+g1calc = data_dict( (r1, Gcalc1), samplename  = 'G1', scan = 'Calc')
 
-#Example of not reseting the marker / color / linestyle, unless the user wants to
-g3 = data( (r1, Gtrunc1), samplename ='G3', scan = 'Orginal')
-g1calc = data( (r1, Gcalc1), samplename  = 'G1', scan = 'Calc', color = 'r')
+# Setup Data for the plot: user can use the group style sheet
+g2 = data_dict( (r2, Gtrunc2), samplename ='G2', scan = 'Orginal', color = 'r', marker = 'o', linestyle = "--")
+g2calc = data_dict( (r2, Gcalc2), samplename = 'G2', scan = 'Calc2', linestyle = "--")
+g2diff = data_dict( (r2, Gdiff2), samplename = 'G2', scan = 'Diff', color = c[0], linestyle = "-")
 
-# plot axis setup: here we have 2 rows x 2 col to test the x and y range setters
-H = Splot(2,2)
+# plot axis setup: here we have 2 rows x 1 col
+H = Splot(2, 1)
 
 # plot data
-H.plotData(g1, 0, 0)
-#Test the exception handling in diffC(). Now there is only 1 curve in the subplot(0,0)
-#H.diffC(0,0)
-H.plotData(g1calc, 0, 0, diff = True)
+H.plot_data(**g1, r=0, c=0)
+H.plot_data(**g1calc, r=0, c=0, diff = True)
+#Test the diffC() offset:
+H.diff_c(0,0, offset = 0)
+H.plot_data(**g1diff, r=0, c=0, offsety = -4)
 
-#Test the diffC() offset = 0:
-#H.diffC(0,0, offset = 0)
-H.plotData(g1diff, 0, 0, offsety = -6)
+H.plot_data(**g2, r=1, c=0)
+H.plot_data(**g2calc, r=1, c=0, diff = True, legend='in')
+H.diff_c(r=1,c=0, offset = 0)
+H.plot_data(**g2diff, r=1, c=0, offsety = -6, legend = 'in')
 
-#Test the exception handling in diffC(). Now there are 3 curves in the subplot(0,0)
-#H.diffC(0,0)
-
-#g2 data uses default set from the group style sheet
-H.plotData(g2, 1, 0)
-#H.plotData(g2calc, 1, 0, diff = True)
-H.plotData(g2calc, 1, 0, diff = False)
-H.plotData(g2diff, 1, 0, offsety = -6, legend = 'in') # Add legend for all curves
-
-#Adjust the figure size to make all lables clear
-H.figureSize(8, 8)
-#H.save(name = "myplot13", form = "pdf")
-
-#Add Title, label
-#H.title("G(r) Plot", math = "off")
-#H.label(x = "hello", xunit = 'delta', y = 'world', yunit = 'epsilon', math = 'on')
-
-#Save figure:
-#H.save(name = "myfigure", form = "pdf")
-
-#Test the range setters: change the x range in the 2nd col
-H.setXLim(1, -4, 4) #change the x range in the 2nd col
-H.setYLim(1, -15, 15) #change the y range in the 2nd row
+#change the figure aspect ratio:
+H.figure_size(6, 8)
