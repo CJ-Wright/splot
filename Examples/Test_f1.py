28e62884a84a5d46ce3a9cf71b0f1c3361cd1c14
Shuyue Xue <sx2174@columbia.edu>
2017-06-27 20:55:00 +0000

    Moving the tests repo.

---
+++ Examples/Test_f1.py
@@ -0,0 +1,71 @@
+import sys
+sys.path.append('../splot')
+from splot1Py3 import *
+
+#get the dataPaths:
+dataPath1 = "../data/examples/f1/80"
+dataPath2 = "../data/examples/f1/10"
+
+#### Load Data Set 1:
+r1 = np.loadtxt(dataPath1, skiprows=4, usecols=(0,))
+Gtrunc1 = np.loadtxt(dataPath1, skiprows=4, usecols=(1,))
+Gdiff1 = np.loadtxt(dataPath1, skiprows=4, usecols=(2,))
+Gcalc1 = np.loadtxt(dataPath1, skiprows=4, usecols=(3,))
+
+# Load Data Set 2:
+r2 = np.loadtxt(dataPath2, skiprows=4, usecols=(0,))
+Gtrunc2 = np.loadtxt(dataPath2, skiprows=4, usecols=(1,))
+Gdiff2 = np.loadtxt(dataPath2, skiprows=4, usecols=(2,))
+Gcalc2 = np.loadtxt(dataPath2, skiprows=4, usecols=(3,))
+
+#Setup Data for the plot:
+#g1 = data( (r1, Gtrunc1), samplename ='G1', scan = 'Orginal', color = c[0], marker = '+')
+g1 = data( (r1, Gtrunc1), samplename ='G1', scan = 'Orginal', color = c[1], marker = 'o')
+#g1calc = data( (r1, Gcalc1), samplename  = 'G1', scan = 'Calc', color = c[4], line='--', marker = "")
+g1calc = data( (r1, Gcalc1), samplename  = 'G1', scan = 'Calc', color = 'r')
+g1diff = data( (r1, Gdiff1), samplename = 'G1', scan = 'Diff', color = c[0])
+
+g2 = data( (r2, Gtrunc2), samplename ='G2', scan = 'Orginal', color = c[2], marker = 'o')
+g2calc = data( (r2, Gcalc2), samplename = 'G2', scan = 'Calc2', color = 'r')
+g2diff = data( (r2, Gdiff2), samplename = 'G2', scan = 'Diff', color = c[0])
+
+#Example of not reseting the marker / color / linestyle, unless the user wants to
+g3 = data( (r1, Gtrunc1), samplename ='G3', scan = 'Orginal')
+g1calc = data( (r1, Gcalc1), samplename  = 'G1', scan = 'Calc', color = 'r')
+
+# plot axis setup: here we have 2 rows x 2 col to test the x and y range setters
+H = Splot(2,2)
+
+# plot data
+H.plotData(g1, 0, 0)
+#Test the exception handling in diffC(). Now there is only 1 curve in the subplot(0,0)
+#H.diffC(0,0)
+H.plotData(g1calc, 0, 0, diff = True)
+
+#Test the diffC() offset = 0:
+#H.diffC(0,0, offset = 0)
+H.plotData(g1diff, 0, 0, offsety = -6)
+
+#Test the exception handling in diffC(). Now there are 3 curves in the subplot(0,0)
+#H.diffC(0,0)
+
+#g2 data uses default set from the group style sheet
+H.plotData(g2, 1, 0)
+#H.plotData(g2calc, 1, 0, diff = True)
+H.plotData(g2calc, 1, 0, diff = False)
+H.plotData(g2diff, 1, 0, offsety = -6, legend = 'in') # Add legend for all curves
+
+#Adjust the figure size to make all lables clear
+H.figureSize(8, 8)
+#H.save(name = "myplot13", form = "pdf")
+
+#Add Title, label
+#H.title("G(r) Plot", math = "off")
+#H.label(x = "hello", xunit = 'delta', y = 'world', yunit = 'epsilon', math = 'on')
+
+#Save figure:
+#H.save(name = "myfigure", form = "pdf")
+
+#Test the range setters: change the x range in the 2nd col
+H.setXLim(1, -4, 4) #change the x range in the 2nd col
+H.setYLim(1, -15, 15) #change the y range in the 2nd row
