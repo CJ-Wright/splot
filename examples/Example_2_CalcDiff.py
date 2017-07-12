f8e9c07cedd4e1d7cb163cb1cf3779d607acc335
Shuyue Xue <sx2174@columbia.edu>
2017-07-12 21:37:18 +0000

    Replace Example_2_CalcDiff.py

--- examples/Example_2_CalcDiff.py
+++ examples/Example_2_CalcDiff.py
@@ -1,6 +1,5 @@
-import sys  
-sys.path.append('../splot')
-from splot import *
+from splot.splot import data_dict, Splot, c
+import numpy as np
 
 #### Load Data Set 1:
 dataPath1 = "data/2/sub_20161114-235343_Ag0_ct_180_2c77c1_0001.gr"
@@ -8,8 +7,8 @@ r1 = np.loadtxt(dataPath1, skiprows=27, usecols=(0,))
 G1 = np.loadtxt(dataPath1, skiprows=27, usecols=(1,))
 # Load Data Set 2:
 dataPath2 = "data/2/sub_20161115-002440_Ag100_ct_180_c73701_0001.gr"    
-r1 = np.loadtxt(dataPath1, skiprows=27, usecols=(0,))
-G1 = np.loadtxt(dataPath1, skiprows=27, usecols=(1,))
+r2 = np.loadtxt(dataPath2, skiprows=27, usecols=(0,))
+G2 = np.loadtxt(dataPath2, skiprows=27, usecols=(1,))
 
 #Setup Data for the plot:
 data_set1 = data_dict( (r1, G1), samplename ='G1', scan = 'Data')
@@ -22,3 +21,6 @@ H = Splot()
 # plot data
 H.plot_data(**data_set1)
 H.plot_data(**data_set2, legend = 'in', diff = True)
+
+#see the figure 
+H.show()
